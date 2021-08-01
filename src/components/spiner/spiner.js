32e344d77569802629774c6a2860b3b6a07e8e5f
unknown <ilya.trofimov.01@gmail.com>
2021-08-01 18:08:47 +0300

    added spiner

---
+++ src/components/spiner/spiner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './spiner.css'
+
+const Spiner = ()=>{
+    return (
+        <div className ='spiner d-flex'>
+            <div class="loadingio-spinner-rolling-bugc4lrxwsq"><div class="ldio-avd98ktfl4">
+            <div></div>
+            </div></div>
+        </div>
+    )
+}
+export default Spiner
